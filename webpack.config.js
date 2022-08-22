d34c3a217b300e1cc3232b0b6b0745d0b55876fa
RadiantGH <iangrapeo@gmail.com>
2022-08-22 13:40:08 -0700

    Devserver and server.js setup.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    // mode: 'development',
+    // mode: 'production',
+    mode: 'development',
     entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          inject: false
+        })
+    ],
+    devServer: {
+        proxy: {
+          '/api': 'http://localhost:3000'
+        },
+        port: 8080,
+        historyApiFallback: true
+    },
 
     module: {
         rules: [
