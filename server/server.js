42fd3f4aa50b6c20bcab95b57fba16d7a2bde215
RadiantGH <iangrapeo@gmail.com>
2022-08-24 01:26:15 -0700

    Removed error handler. Added handler for react router.

--- server/server.js
+++ server/server.js
@@ -13,10 +13,14 @@ app.get('/api/assets/talk_typescript.mp3', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '../src/assets/talk_typescript.mp3'));
 })
 
-//Error
-app.use('/', (req, res) => {
-    res.status(404).send('404 not found');
+app.get('*', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
+
+// //Error
+// app.get('/', (req, res) => {
+//     res.status(404).send('404 not found');
+// });
 
 app.listen(port, () => {
     console.log('Started server listening on port: ', port);
