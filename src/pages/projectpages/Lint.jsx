1b68fb5677df4a693582abe867bab10704a87f4d
RadiantGH <iangrapeo@gmail.com>
2022-08-24 10:34:56 -0700

    Added SeeQR page.

---
+++ src/pages/projectpages/Lint.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Article_Lint = function() {
+    return (
+        <div className='article-content'>
+            
+        </div>
+    );
+}
+
+export default Article_Lint;
