bc23e95fe909efab5029d94ea6449678eae938f5
RadiantGH <iangrapeo@gmail.com>
2022-08-27 18:49:27 -0700

    Added topic for seeqr article.

--- src/pages/projectpages/Lint.jsx
+++ src/pages/projectpages/Lint.jsx
@@ -9,6 +9,16 @@ const Article_Lint = function() {
                 SeeQR is a query analytics tool for Postgres and recently MySQL. My team and I focused our efforts on implementing MySQL and laying the groundwork 
                 for adding future RDBMS. 
             </p>
+
+            <strong>Table of Contents</strong>
+            <ol>
+                <li><a href='#hardcoded' className='url'>The Existing Implementation</a></li>
+            </ol>
+
+            <h3 id='hardcoded'>The Existing Implementation</h3>
+            <p>
+                Content
+            </p>
         </div>
     );
 }
