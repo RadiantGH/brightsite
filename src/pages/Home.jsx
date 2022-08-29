2e39345be9c2fb5a22002ea4fe33e1d2e95b6e36
RadiantGH <iangrapeo@gmail.com>
2022-08-29 12:01:01 -0700

    Minor homepage edits.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ const Home = function() {
             <div id='home-body'>
             <p>
                 I'm Ian (aka Radiant). I'm the primary developer for <a href='/shard' className='url'>Treasure Seeker</a>, a cardgame based on the late Runescape: Chronicle. 
-                I'm originally from the Philippines and now live in LA County, California. I'm passionate about teaching Computer Science to aspiring developers, camping, abandoned places, and playing fighting games with a keyboard.
+                I'm originally from the Philippines and now live in around Los Angeles, California. I'm passionate about teaching Computer Science to aspiring developers, camping, abandoned places, and playing fighting games with a keyboard.
                 <br/><br/>
-                My latest projects include <a href='/lint' className='url'>SeeQR</a>, a SQL query analytics and optimization tool I worked on under OSLabs - and <a href='/worm' className='url'>Nydus</a>, my tag based media organization app inspired by Hydrus Network.
+                My latest projects include <a href='/lint' className='url'>SeeQR</a>, a SQL query analytics and optimization tool I worked on under OSLabs - and <a href='/worm' className='url'>Nydus</a>, my tag based media organization app.
                 <br/><br/>
                 I was recently invited by SingleSprout to give a <a href='/talks' className='url'>talk on TypeScript</a> - and I'm hoping to do more in the future!
             </p>
