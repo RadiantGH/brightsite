9b0fee72642e0654c06da1c69badbae126fa5778
RadiantGH <iangrapeo@gmail.com>
2022-08-29 12:24:55 -0700

    Grammar.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = function() {
             <div id='home-body'>
             <p>
                 I'm Ian (aka Radiant). I'm the primary developer for <a href='/shard' className='url'>Treasure Seeker</a>, a cardgame based on the late Runescape: Chronicle. 
-                I'm originally from the Philippines and now live in around Los Angeles, California. I'm passionate about teaching Computer Science to aspiring developers, camping, abandoned places, and playing fighting games with a keyboard.
+                I'm originally from the Philippines and now live around Los Angeles, California. I'm passionate about teaching Computer Science to aspiring developers, camping, abandoned places, and playing fighting games with a keyboard.
                 <br/><br/>
                 My latest projects include <a href='/lint' className='url'>SeeQR</a>, a SQL query analytics and optimization tool I worked on under OSLabs - and <a href='/worm' className='url'>Nydus</a>, my tag based media organization app.
                 <br/><br/>
